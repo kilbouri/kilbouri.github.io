5f17b83da3759cb8aada704bc45f51dd7124502b
Void <voidvenommc@gmail.com>
2021-06-03 21:22:33 -0400

    Added language icons to cards
    Languages are now cached on the repl caching server. The url-compatible language name ("C#" -> "csharp") is used in a github user content URL (PKeif's Material Icons for VS Code) to show an appropriate icon in the bottom right of each card.
    
    Also refactored external-data.js to reduce repetition.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # kilbouri.github.io
-My personal, custom website built by me and hosted for free by the amazing people at GitHub! Still very much in development, but so far I'm very happy with how things are shaping up.
+
+My personal, custom website built by me and hosted for free by the amazing people at GitHub!
--- scripts/external-data.js
+++ scripts/external-data.js
@@ -1,87 +1,120 @@
-let months = ["Jan.", "Feb.", "Mar.", "Apr.", "May", "Jun.", "Jul.", "Aug.", "Sept.", "Oct.", "Nov.", "Dec."];
-let repo_template = document.getElementById("repo_template");
+let months = ['Jan.', 'Feb.', 'Mar.', 'Apr.', 'May', 'Jun.', 'Jul.', 'Aug.', 'Sept.', 'Oct.', 'Nov.', 'Dec.'];
+let repo_template = document.getElementById('repo_template');
+let language_overrides = {
+  python3: 'python',
+  python2: 'python',
+};
+
+function get_name(name, isFork) {
+  if (isFork) return `${name} (fork)`;
+  else return name;
+}
+
+function interpret_date(date) {
+  if (date == undefined) return '';
+  date = new Date(date);
+
+  let h = String(date.getHours() % 12);
+  let m = String(date.getMinutes()).padEnd(2, '0');
+  let x = date.getHours() > 12 ? 'pm' : 'am';
+
+  let D = date.getDay() + 1;
+  let M = months[date.getMonth()];
+  let Y = date.getFullYear();
+
+  return `Last update: ${M} ${D}, ${Y} at ${h}:${m}${x}`;
+}
+
+function get_language_url(lang) {
+  let langName = get_language_name(lang);
+  if (langName) return `https://raw.githubusercontent.com/PKief/vscode-material-icon-theme/main/icons/${langName}.svg`;
+  else return '';
+}
+
+function get_language_name(lang) {
+  lang = String(lang);
+  if (lang == 'null' || lang == 'undefined') return '';
+
+  lang = lang.replace('#', 'sharp');
+  lang = lang.toLowerCase();
+  if (lang in language_overrides) return language_overrides[lang];
+
+  return lang;
+}
 
 function add_repo(parent, json) {
-	let new_clone = repo_template.content.cloneNode(true);
+  let new_clone = repo_template.content.cloneNode(true);
 
-	// set the repo name
-	let name = json.name;
-	new_clone.querySelector(".repo-name").innerHTML = name ? name : "Un-named Repository";
-	
-	// set the repo link
-	let url = json.html_url;
-	if (url != undefined)
-		new_clone.querySelector(".repo-link").href = url;
-	else
-		new_clone.querySelector(".repo-link").target = "";
-		
-	// set the repo description
-	let description = json.description;
-	new_clone.querySelector(".repo-desc").innerHTML = description ? description : "No description";
-	
-	// set the repo's last push date and time
-	if (json.pushed_at != undefined) {
-		let last_push = new Date(json.pushed_at);
-		let time = last_push.getHours() % 12 + ":" + String(last_push.getMinutes()).padEnd(2, "0") + ((last_push.getHours() > 12) ? "pm" : "am");
-		let date = months[last_push.getMonth()] + " " + (last_push.getDay() + 1) + ", " + last_push.getFullYear();
-		new_clone.querySelector(".repo-push").innerHTML = "Last updated at " + time + " on " + date;
-	} else 
-		new_clone.querySelector(".repo-push").innerHTML = "Failed to fetch date."
-	
-	// append the repo to the grid
-	parent.appendChild(new_clone);
+  new_clone.querySelector('#repo-name').innerHTML = get_name(json.name, json.fork);
+  new_clone.querySelector('#repo-link').href = json.html_url;
+  new_clone.querySelector('#repo-desc').innerHTML = json.description;
+  new_clone.querySelector('#repo-push').innerHTML = interpret_date(json.pushed_at);
+  new_clone.querySelector('#repo-lang').src = get_language_url(json.language);
+  new_clone.querySelector('#repo-lang').title = get_language_name(json.language);
+
+  // append the repo to the grid
+  parent.appendChild(new_clone);
 }
 
 // fetch my repo list from the GitHub API
-fetch("https://api.github.com/users/kilbouri/repos")
-.then(function (response) {
-	return response.json();
-})
-.then(function (json) {
-	let repo_table = document.getElementById("repo_table");
-	json.forEach(element => {
-		add_repo(repo_table, element);
-	});
-})
-.catch(function (error) {
-	let repo_table = document.getElementById("repo_table");
-	add_repo(repo_table, {
-		name: "Unable to reach the GitHub API",
-		html_url: undefined,
-		description: "An unexpected error occurred while fetching my repositories from GitHub. Please try again in a few seconds.",
-		last_push: undefined
-	});
-
-	console.log("GitHub Fetch Error: " + error);
-});
+get_request_json('https://api.github.com/users/kilbouri/repos')
+  .then((json) => {
+    remove_github_spinner();
+    let repo_table = document.getElementById('repo_table');
+    json.forEach((element) => {
+      console.log(element);
+      add_repo(repo_table, element);
+    });
+  })
+  .catch((error) => {
+    remove_github_spinner();
+    add_repo(document.getElementById('repo_table'), get_error_card('GitHub'));
+    console.log(`GitHub Fetch Error -> ${error}`);
+  });
 
 // fetch my cached repl list from the repl server
-fetch("https://repllistserver.voidvenom.repl.co/repls")
-.then(response => {
-	return response.json();
-})
-.then(json => {
-	let items = json.data.userByUsername.publicRepls.items;
-	items.forEach(repl => {
-		// translate the fetched json to the github format
-		let translated = {
-			name: repl.title,
-			html_url: "https://repl.it" + repl.url,
-			description: repl.description,
-			pushed_at: repl.timeUpdated
-		};
-		let repl_table = document.getElementById("repl_table");
-		add_repo(repl_table, translated);	
-	});
-})
-.catch(function (error) {
-	let repl_table = document.getElementById("repl_table");
-	add_repo(repl_table, {
-		name: "Unable to fetch the repl list",
-		html_url: undefined,
-		description: "An unexpected error occurred while fetching my repls from my repl list server. Please try again in a few seconds.",
-		last_push: undefined
-	});
+get_request_json('https://replcacheserver.voidvenom.repl.co')
+  .then((json) => {
+    remove_repl_spinner();
+    json.data.userByUsername.publicRepls.items.forEach((repl) => {
+      add_repo(document.getElementById('repl_table'), {
+        name: repl.title,
+        html_url: `https://repl.it${repl.url}`,
+        description: repl.description,
+        pushed_at: repl.timeUpdated,
+        language: repl.language,
+        fork: false,
+      });
+    });
+  })
+  .catch((error) => {
+    add_repo(document.getElementById('repl_table'), get_error_card('Repl Cache Server'));
+    remove_repl_spinner();
+    console.log(`Repl Fetch Error -> ${error}`);
+  });
 
-	console.log("Repl Fetch Error: " + error);
-});
+async function get_request_json(url) {
+  return fetch(url).then((response) => {
+    return response.json();
+  });
+}
+
+function get_error_card(source) {
+  return {
+    name: `Unable to reach ${source}`,
+    html_url: undefined,
+    description: 'Please try again in a few seconds.',
+    last_push: undefined,
+    language: undefined,
+  };
+}
+
+function remove_github_spinner() {
+  let toRemove = document.querySelector('#github-loading-spinner');
+  toRemove.parentNode.removeChild(toRemove);
+}
+
+function remove_repl_spinner() {
+  let toRemove = document.querySelector('#repl-loading-spinner');
+  toRemove.parentNode.removeChild(toRemove);
+}
